8a4d215907a5ccbb09b0e4a296d7a1f6f36b2071
thetryworks <natesmyth1@gmail.com>
2022-07-09 21:20:43 -0500

    intitial commit

--- Book-Search-Engine/server/utils/auth.js
+++ Book-Search-Engine/server/utils/auth.js
@@ -6,9 +6,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function ({ req }) {
+  authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -25,9 +25,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
+      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    return req;
+    // send to next endpoint
+    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
